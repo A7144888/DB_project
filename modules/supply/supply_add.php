6fe5642275d3d54824a6ede9876c8e5bcbfc6854
jojo00857139-creator <jojo00857139@gmail.com>
2025-12-10 20:38:20 +0800

    Update supply_add.php

--- modules/supply/supply_add.php
+++ modules/supply/supply_add.php
@@ -6,10 +6,11 @@
     $spName         = $_POST["spName"];
     $spPhone        = $_POST["spPhone"];
     $spAddress      = $_POST["spAddress"];
+    $spTime         = $_POST["spTime"];
 
-    $query = ("Insert into `supplier` (`spName`,`spPhone`,`spAddress`) values (?,?,?)");
+    $query = ("Insert into `supplier` (`spName`,`spPhone`,`spAddress`,`spTime`) values (?,?,?,?)");
     $stmt = $db->prepare($query);
-    $stmt->bind_param("sss",$spName,$spPhone,$spAddress);
+    $stmt->bind_param("ssss",$spName,$spPhone,$spAddress,$spTime);
     $stmt->execute();
 
     header('Location: supply_edit.php');
